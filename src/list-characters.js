d0776b06daf94cb8c749d0757ce8b3692d399f01
Richard Kotze <richkotze@outlook.com>
2019-12-08 20:22:11 +0000

    Select a value on change

--- src/list-characters.js
+++ src/list-characters.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { useTheFetch } from "./use-the-fetch";
 
-export function ListCharacters() {
+export function ListCharacters({ onChange }) {
   const { loading, data } = useTheFetch("people");
 
+  function selectedCharacter(e) {
+    onChange(data.results[e.target.value]);
+  }
   return (
-    <select>
+    <select role="character-select" onChange={selectedCharacter}>
       {!loading &&
-        data.results.map(character => (
-          <option key={character.name}>{character.name}</option>
+        data.results.map((character, i) => (
+          <option value={i} key={character.name}>
+            {character.name}
+          </option>
         ))}
       {loading && <option>Loading ...</option>}
     </select>
--- src/list-characters.spec.js
+++ src/list-characters.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import { ListCharacters } from "./list-characters";
 import { useTheFetch } from "./use-the-fetch";
@@ -43,5 +43,43 @@ describe("list characters", () => {
     expect(useTheFetch).toBeCalledWith("people");
     expect(container).toHaveTextContent("Luke Skywalker");
     expect(selectList).toHaveLength(3);
+  });
+
+  it("select a different character to get details", () => {
+    useTheFetch.mockReturnValue({
+      loading: false,
+      data: {
+        results: [
+          {
+            name: "Luke Skywalker",
+            height: "177"
+          },
+          {
+            name: "C-3PO",
+            height: "169"
+          },
+          {
+            name: "Darth Vader",
+            height: "201"
+          }
+        ]
+      }
+    });
+    const handleNewCharacter = jest.fn();
+
+    const { getByRole } = render(
+      <ListCharacters onChange={handleNewCharacter} />
+    );
+
+    fireEvent.change(getByRole("character-select"), {
+      target: {
+        value: 2
+      }
+    });
+
+    expect(handleNewCharacter).toBeCalledWith({
+      name: "Darth Vader",
+      height: "201"
+    });
   });
 });
